f17e2448885afcea362969ae545c071c7dbfe03f
UlyssePerret <ulysseperret@orange.fr>
2023-04-28 13:31:09 +0200

    Delete javadoc directory

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calendrier"},{"p":"<Unnamed>","l":"Evenement"},{"p":"<Unnamed>","l":"FlipVcards"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Vcard"},{"p":"<Unnamed>","l":"Vcard_nik"}]
