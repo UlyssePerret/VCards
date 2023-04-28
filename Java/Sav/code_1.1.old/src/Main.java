87592c31561cd9993afb04efb8f71a645c73e331
UlyssePerret <ulysseperret@orange.fr>
2023-04-28 11:33:12 +0200

    Add files via upload
    Prep codes

---
+++ Java/Sav/code_1.1.old/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;//import
+
+/**Changelog
+ * Creation le 23 janvier 2020
+ * Mise a jour : le 1 février 202à
+ * 1.0 : Creation Main pour test
+ * 1.1.3 Samedi 1 février
+ * - Ajout fonction Menu() , pour une meilleur interface utilisateur
+ *
+ * 1.1.4 Mecredi 5 janvier
+ * utilisation du constructeur vcard
+ * 
+ * 1.5.1 Samedi 7 février
+ * mise en forme + commentaire
+ *
+ * V1.1.6.0 - Mecredi 12 Fevrier
+ * Relecture
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    Vcard vcard = new Vcard(); //permet de lancer le constructeur Vcard et donc le menu contenu
+
+    }
+}
